b03ab2785437738b069e7e44350c5de7acf185c9
Francesc Verdugo <fverdugo@cimne.upc.edu>
2022-01-20 17:26:32 +0100

    Removing exports

--- src/GridapMHD.jl
+++ src/GridapMHD.jl
@@ -9,16 +9,6 @@ using Gridap.CellData
 using Gridap.ReferenceFEs
 using Gridap.Geometry
 
-export ConductingFluid
-export MagneticField
-export FluidForce
-export VelocityBc
-export TractionBc
-export CurrentBc
-export PotentialBc
-export FluidForce
-export ConductingThinWall
-
 include("Main.jl")
 
 include("Hunt.jl")
