2eb18d3a2dba0f2f17c73f58b995f664f72416ec
Francesc Verdugo <fv3851@gadi-login-03.gadi.nci.org.au>
2022-02-16 22:07:41 +1100

    Adding env.sh script

---
+++ analysis/scripts/env.sh
@@ -0,0 +1,8 @@
+module load openmpi/4.1.2
+module load intel-mkl/2022.0.2
+module load julia/1.7.1
+export PETSCROOT=/home/552/fv3851/Apps/petsc/3.15.5
+export JULIA_PETSC_LIBRARY=/home/552/fv3851/Apps/petsc/3.15.5/lib/libpetsc.so
+export JULIA_MPI_BINARY=system
+export JULIA_MPI_PATH=/apps/openmpi/4.1.2
+
