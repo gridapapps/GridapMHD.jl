1ca55f565af40ea3ecb5a21a2344bbeb819ad3e4
Francesc Verdugo <fverdugo@cimne.upc.edu>
2022-02-16 22:19:56 +1100

    Added compilation script [skip-ci]

---
+++ analysis/scripts/compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -q normal 
+#PBS -l walltime=00:40:00
+#PBS -l ncpus=1
+#PBS -l mem=6gb
+#PBS -N compile
+#PBS -l wd
+
+source env.sh
+
+julia --project=$GRIDAPMHD -O3 --check-bounds=no --color=yes $GRIDAPMHD/compile/compile.jl
+
