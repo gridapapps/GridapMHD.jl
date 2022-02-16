fd2bb681b8d0a0ffc1878db714c0dbcdf8d71204
Francesc Verdugo <fverdugo@cimne.upc.edu>
2022-02-17 04:04:41 +1100

    Adding user-installed julia

--- analysis/scripts/env.sh
+++ analysis/scripts/env.sh
@@ -1,9 +1,9 @@
 module load openmpi/4.1.2
 module load intel-mkl/2022.0.2
-module load julia/1.7.1
 export PETSCROOT=/home/552/fv3851/Apps/petsc/3.15.5
 export JULIA_PETSC_LIBRARY=/home/552/fv3851/Apps/petsc/3.15.5/lib/libpetsc.so
 export JULIA_MPI_BINARY=system
 export JULIA_MPI_PATH=/apps/openmpi/4.1.2
 export GRIDAPMHD=/scratch/bt62/fv3851/GridapMHD.jl
+export PATH=/home/552/fv3851/Apps/julia/1.7.2/bin:$PATH
 #export UCX_WARN_UNUSED_ENV_VARS=n
