d09d3866fc8931ef83db999b0a0e486ea44923d2
Francesc Verdugo <fverdugo@cimne.upc.edu>
2022-02-21 09:29:09 +0100

    More work prior experiements

--- compile/warmup.jl
+++ compile/warmup.jl
@@ -6,8 +6,13 @@ hunt(
   B=(0.,50.,0.),
   debug=false,
   vtk=true,
+  nsums=50,
+  path=".",
+  nruns=1,
+  verbose=true,
   title="warmup_hunt",
   solver=:petsc,
+  petsc_options="-snes_monitor -ksp_error_if_not_converged true -ksp_converged_reason -ksp_type preonly -pc_type lu -pc_factor_mat_solver_type mumps"
 )
 
 hunt(
@@ -18,6 +23,11 @@ hunt(
   B=(0.,50.,0.),
   debug=false,
   vtk=true,
+  nsums=50,
+  path=".",
+  nruns=1,
+  verbose=true,
   title="warmup_hunt_mpi",
   solver=:petsc,
+  petsc_options="-snes_monitor -ksp_error_if_not_converged true -ksp_converged_reason -ksp_type preonly -pc_type lu -pc_factor_mat_solver_type mumps"
 )
