ab89239834d44a054d57e73ea616fca8137d0139
Francesc Verdugo <fverdugo@cimne.upc.edu>
2022-02-23 01:35:24 +1100

    Saving changes

--- analysis/scripts/preparejobs.jl
+++ analysis/scripts/preparejobs.jl
@@ -15,7 +15,8 @@ function jobtitle(params)
   nr = params[:nr]
   ns = params[:ns]
   ps = params[:ps]
-  title="ha$(tostr(ha,5))cx$(tostr(cx,3))px$(tostr(px,2))ns$(tostr(ns,3))ps$(tostr(ps,2))"
+  km = params[:km]
+  title="ha$(tostr(ha,5))cx$(tostr(cx,3))px$(tostr(px,2))ns$(tostr(ns,3))ps$(tostr(ps,2))km$(tostr(km,1))"
 end
 
 function petsc_options(i)
@@ -26,13 +27,16 @@ function petsc_options(i)
   end
 end
 
+# Small convergence test
+
 allparams = Dict(
  :ha=>[500],
  :cx=>[15,30,45,64,90,129,179],
  :px=>[2],
  :nr=>[1],
  :ns=>[100],
- :ps=>[1])
+ :ps=>[1],
+ :km=>[1])
 
 params = dict_list(allparams)
 dicts = map(params) do params
@@ -42,13 +46,14 @@ dicts = map(params) do params
   nr = params[:nr]
   ns = params[:ns]
   ps = params[:ps]
+  km = params[:km]
   title=jobtitle(params)
   Dict(
    :q=>"normal",
    :walltime=>"00:30:00",
    :ncpus=>px^2,
    :mem=>"180gb",
-   :jobfs=>"2gb",
+   :jobfs=>"1gb",
    :name=>title,
    :n=>px^2,
    :nc=>(cx,cx),
@@ -60,6 +65,7 @@ dicts = map(params) do params
    :path=>datadir(),
    :nruns=>nr,
    :nsums=>ns,
+   :kmap=>km,
    :petsc_options=>petsc_options(ps),
   )
 end
@@ -73,4 +79,68 @@ for dict in dicts
   end
 end
 
+# Scaling
+
+allparams = Dict(
+ :ha=>[500],
+ :cx=>[179],
+ :px=>[2,3,4,6,8,12,16],
+ :nr=>[2],
+ :ns=>[100],
+ :ps=>[1],
+ :km=>[1])
+
+function get_walltime(tmax,tmin,n)
+  msmax = Millisecond(tmax)
+  msmin = Millisecond(tmin)
+  ms = Millisecond(ceil(msmax.value/n))
+  s = convert(Time,convert(DateTime,ms))
+  smin = convert(Time,convert(DateTime,msmin))
+  s < smin ? smin : s
+end
+
+params = dict_list(allparams)
+dicts = map(params) do params
+  cx = params[:cx]
+  px = params[:px]
+  ha = params[:ha]
+  nr = params[:nr]
+  ns = params[:ns]
+  ps = params[:ps]
+  km = params[:km]
+
+  n = px^2
+  wt = Hour(1)
+  nnodes = ceil(n/48) |> Int
+  title=jobtitle(params)
+  Dict(
+   :q=>"normal",
+   :walltime=>get_walltime(Hour(1),Minute(30),n),
+   :ncpus=>48*nnodes,
+   :mem=>"$(185*nnodes)gb",
+   :jobfs=>"1gb",
+   :name=>title,
+   :n=>n,
+   :nc=>(cx,cx),
+   :np=>(px,px),
+   :B=>(0.,Float64(ha),0.),
+   :debug=>"false",
+   :vtk=>"false",
+   :title=>title,
+   :path=>datadir(),
+   :nruns=>nr,
+   :nsums=>ns,
+   :kmap=>km,
+   :petsc_options=>petsc_options(ps),
+  )
+end
+
+template = read(scriptsdir("jobtemplate"),String)
+
+for dict in dicts
+  jobfile = datadir(dict[:name]*".sh")
+  open(jobfile,"w") do io
+    render(io,template,dict|>tostringdict)
+  end
+end
 
