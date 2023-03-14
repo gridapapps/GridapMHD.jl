b247ef1badd6f2f3378a0cf8fb240e71dfa2ff4c
principe <principe@cimne.upc.edu>
2023-03-14 19:21:25 +0100

    Added generation of weak scaling tests in preparejobs

--- analysis/scripts/preparejobs.jl
+++ analysis/scripts/preparejobs.jl
@@ -4,8 +4,8 @@ using Dates
 
 mkpath(datadir())
 
-function tostr(v,n)
-  lpad(v,n,'0')
+function tostr(v, n)
+  lpad(v, n, '0')
 end
 
 function jobtitle(params)
@@ -16,7 +16,7 @@ function jobtitle(params)
   ns = params[:ns]
   ps = params[:ps]
   km = params[:km]
-  title="ha$(tostr(ha,5))cx$(tostr(cx,3))px$(tostr(px,2))ns$(tostr(ns,3))ps$(tostr(ps,2))km$(tostr(km,1))"
+  title = "ha$(tostr(ha,5))cx$(tostr(cx,3))px$(tostr(px,2))ns$(tostr(ns,3))ps$(tostr(ps,2))km$(tostr(km,1))"
 end
 
 function petsc_options(i)
@@ -27,142 +27,208 @@ function petsc_options(i)
   end
 end
 
-function preparjobs_conv()
+function preparejobs_conv()
 
-# Small convergence test
+  # Small convergence test
 
-# allparams = Dict(
-#  :ha=>[500],
-#  :cx=>[15,30,45,64,90,129,179],
-#  :px=>[2],
-#  :nr=>[1],
-#  :ns=>[100],
-#  :ps=>[1],
-#  :km=>[1])
-# Take points equally spaced in a log plot 
-#log_cx=[1,1.25,1.5,1.75,2,2.25]
-#cx=map(x->Int(round(x)),10 .^ log_cx)
+  # allparams = Dict(
+  #  :ha=>[500],
+  #  :cx=>[15,30,45,64,90,129,179],
+  #  :px=>[2],
+  #  :nr=>[1],
+  #  :ns=>[100],
+  #  :ps=>[1],
+  #  :km=>[1])
+  # Take points equally spaced in a log plot 
+  #log_cx=[1,1.25,1.5,1.75,2,2.25]
+  #cx=map(x->Int(round(x)),10 .^ log_cx)
 
-allparams = Dict(
-  :ha=>[50],
-  :cx=>[400,800],
-  :px=>[2],
-  :nr=>[1],
-  :ns=>[500],
-  :ps=>[1],
-  :km=>[1])
- 
-params = dict_list(allparams)
-dicts = map(params) do params
-  cx = params[:cx]
-  px = params[:px]
-  ha = params[:ha]
-  nr = params[:nr]
-  ns = params[:ns]
-  ps = params[:ps]
-  km = params[:km]
-  title=jobtitle(params)
-  Dict(
-   :q=>"hugemem",
-   :walltime=>"04:00:00",
-   :ncpus=>px^2,
-   :mem=>"180gb",
-   :jobfs=>"1gb",
-   :name=>title,
-   :n=>px^2,
-   :nc=>(cx,cx),
-   :np=>(px,px),
-   :B=>(0.,Float64(ha),0.),
-   :debug=>"false",
-   :vtk=>"false",
-   :title=>title,
-   :path=>datadir(),
-   :nruns=>nr,
-   :nsums=>ns,
-   :kmap=>km,
-   :petsc_options=>petsc_options(ps),
-  )
-end
+  allparams = Dict(
+    :ha => [50],
+    :cx => [10,18,32,56,100,178,400],
+    :px => [2],
+    :nr => [1],
+    :ns => [500],
+    :ps => [1],
+    :km => [1])
 
-template = read(scriptsdir("jobtemplate"),String)
-
-for dict in dicts
-  jobfile = datadir(dict[:name]*".sh")
-  open(jobfile,"w") do io
-    render(io,template,dict|>tostringdict)
+  params = dict_list(allparams)
+  dicts = map(params) do params
+    cx = params[:cx]
+    px = params[:px]
+    ha = params[:ha]
+    nr = params[:nr]
+    ns = params[:ns]
+    ps = params[:ps]
+    km = params[:km]
+    title = jobtitle(params)
+    Dict(
+      :q => "hugemem",
+      :walltime => "04:00:00",
+      :ncpus => px^2,
+      :mem => "180gb",
+      :jobfs => "1gb",
+      :name => title,
+      :n => px^2,
+      :nc => (cx, cx),
+      :np => (px, px),
+      :B => (0.0, Float64(ha), 0.0),
+      :debug => "false",
+      :vtk => "false",
+      :title => title,
+      :path => datadir(),
+      :nruns => nr,
+      :nsums => ns,
+      :kmap => km,
+      :petsc_options => petsc_options(ps),
+    )
   end
+
+  template = read(scriptsdir("jobtemplate"), String)
+
+  for dict in dicts
+    jobfile = datadir(dict[:name] * ".sh")
+    open(jobfile, "w") do io
+      render(io, template, dict |> tostringdict)
+    end
+  end
+
+  return nothing
 end
 
-return nothing
-end
-
-function get_walltime(tmax,tmin,n)
+function get_walltime(tmax, tmin, n)
   msmax = Millisecond(tmax)
   msmin = Millisecond(tmin)
-  ms = Millisecond(ceil(msmax.value/n))
-  s = convert(Time,convert(DateTime,ms))
-  smin = convert(Time,convert(DateTime,msmin))
+  ms = Millisecond(ceil(msmax.value / n))
+  s = convert(Time, convert(DateTime, ms))
+  smin = convert(Time, convert(DateTime, msmin))
   s < smin ? smin : s
 end
 
 # Strong Scaling
 function preparejobs_ssca()
-allparams = Dict(
- :ha=>[50],
- :cx=>[400],
- :px=>[3,4,6,8,12,16],
- :nr=>[2],
- :ns=>[1],
- :ps=>[1],
- :km=>[1])
+  allparams = Dict(
+    :ha => [50],
+    :cx => [400],
+    :px => [2, 3, 4, 6, 8, 12, 16],
+    :nr => [2],
+    :ns => [1],
+    :ps => [1],
+    :km => [1])
 
-params = dict_list(allparams)
-dicts = map(params) do params
-  cx = params[:cx]
-  px = params[:px]
-  ha = params[:ha]
-  nr = params[:nr]
-  ns = params[:ns]
-  ps = params[:ps]
-  km = params[:km]
+  params = dict_list(allparams)
+  dicts = map(params) do params
+    cx = params[:cx]
+    px = params[:px]
+    ha = params[:ha]
+    nr = params[:nr]
+    ns = params[:ns]
+    ps = params[:ps]
+    km = params[:km]
 
-  n = px^2
-  wt = Hour(1)
-  nnodes = ceil(n/48) |> Int
-  title=jobtitle(params)
-  Dict(
-   :q=>"hugemem",
-   :walltime=>get_walltime(Hour(18),Minute(80),n),
-   :ncpus=>48*nnodes,
-   :mem=>"$(190*nnodes)gb",
-   :jobfs=>"1gb",
-   :name=>title,
-   :n=>n,
-   :nc=>(cx,cx),
-   :np=>(px,px),
-   :B=>(0.,Float64(ha),0.),
-   :debug=>"false",
-   :vtk=>"false",
-   :title=>title,
-   :path=>datadir(),
-   :nruns=>nr,
-   :nsums=>ns,
-   :kmap=>km,
-   :petsc_options=>petsc_options(ps),
-  )
-end
-
-template = read(scriptsdir("jobtemplate"),String)
-
-for dict in dicts
-  jobfile = datadir(dict[:name]*".sh")
-  open(jobfile,"w") do io
-    render(io,template,dict|>tostringdict)
+    n = px^2
+    nnodes = ceil(n / 48) |> Int
+    title = jobtitle(params)
+    if nnodes>5
+      mem=190*nnodes
+      q="normal"
+    else
+      mem=1000
+      q="hugemem"
+    end
+    Dict(
+      :q => q,
+      :walltime => get_walltime(Hour(20), Minute(120), n),
+      :ncpus => px^2,
+      :mem => "$(mem)gb",
+      :jobfs => "1gb",
+      :name => title,
+      :n => n,
+      :nc => (cx, cx),
+      :np => (px, px),
+      :B => (0.0, Float64(ha), 0.0),
+      :debug => "false",
+      :vtk => "false",
+      :title => title,
+      :path => datadir(),
+      :nruns => nr,
+      :nsums => ns,
+      :kmap => km,
+      :petsc_options => petsc_options(ps),
+    )
   end
+
+  template = read(scriptsdir("jobtemplate"), String)
+
+  for dict in dicts
+    jobfile = datadir(dict[:name] * ".sh")
+    open(jobfile, "w") do io
+      render(io, template, dict |> tostringdict)
+    end
+  end
+
+  return nothing
 end
 
-return nothing
-end
 
+# Weak Scaling
+function preparejobs_wsca()
+  allparams = Dict(
+    :ha => [50],
+    :cx => [16],
+    :px => [2, 3, 4, 6, 8, 12, 16],
+    :nr => [2],
+    :ns => [1],
+    :ps => [1],
+    :km => [1])
+
+  params = dict_list(allparams)
+  dicts = map(params) do params
+    cx = params[:cx]
+    px = params[:px]
+    ha = params[:ha]
+    nr = params[:nr]
+    ns = params[:ns]
+    ps = params[:ps]
+    km = params[:km]
+
+    n = px^2
+    cx = cx * px # So N=cx^2 scales as P=px^2
+    params[:cx]=cx
+    nnodes = ceil(n / 48) |> Int
+    title = jobtitle(params)
+    Dict(
+      :q => "normal",
+      :walltime => "04:00:00",
+      :ncpus => px^2,
+      :mem => "$(190*nnodes)gb",
+      :jobfs => "1gb",
+      :name => title,
+      :n => n,
+      :nc => (cx, cx),
+      :np => (px, px),
+      :B => (0.0, Float64(ha), 0.0),
+      :debug => "false",
+      :vtk => "false",
+      :title => title,
+      :path => datadir(),
+      :nruns => nr,
+      :nsums => ns,
+      :kmap => km,
+      :petsc_options => petsc_options(ps),
+    )
+  end
+
+  template = read(scriptsdir("jobtemplate"), String)
+
+  for dict in dicts
+    jobfile = datadir(dict[:name] * ".sh")
+    open(jobfile, "w") do io
+      render(io, template, dict |> tostringdict)
+    end
+  end
+
+  return nothing
+end
 
 
