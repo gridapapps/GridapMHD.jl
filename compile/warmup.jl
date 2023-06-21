fb914f85e1e3311f6321969e3dbd5ac95d7a67d7
Javier Principe <principe@cimne.upc.edu>
2023-06-22 03:37:56 +1000

    Simplified compilation scripts

--- compile/warmup.jl
+++ compile/warmup.jl
@@ -2,70 +2,36 @@ using GridapMHD: hunt
 using GridapMHD: expansion
 
 hunt(
-  nc=(4,4),
-  L=1.0,
-  B=(0.,50.,0.),
-  debug=false,
-  vtk=true,
-  nsums=50,
-  path=".",
-  nruns=1,
-  verbose=true,
-  title="warmup_hunt_julia",
-  solver=:julia,
-  BL_adapted = true 
-)
-
-hunt(
-  nc=(4,4),
-  L=1.0,
-  B=(0.,50.,0.),
-  debug=false,
-  vtk=true,
-  nsums=50,
-  path=".",
-  nruns=1,
-  verbose=true,
-  title="warmup_hunt_petsc",
-  solver=:petsc,
-  BL_adapted = true,
-  petsc_options="-snes_monitor -ksp_error_if_not_converged true -ksp_converged_reason -ksp_type preonly -pc_type lu -pc_factor_mat_solver_type mumps",
-  kmap_x=1,
-  kmap_y=1,
+  title="warmup_hunt_res_jac",
+  solve=false,
   res_assemble=true,
-  jac_assemble=true,
-  solve=true
+  jac_assemble=true
 )
 
 hunt(
-  nc=(4,4),
+  title="warmup_hunt_julia",
+  solver=:julia
+)
+
+hunt(
+  title="warmup_hunt_petsc",
+  solver=:petsc
+)
+
+hunt(
+  np=(2,2),
+  backend=:sequential,
+  title="warmup_hunt_np4",
+  solver=:petsc
+)
+
+hunt(
   np=(1,1),
   backend=:mpi,
-  L=1.0,
-  B=(0.,50.,0.),
-  debug=false,
-  vtk=true,
-  nsums=50,
-  path=".",
-  nruns=1,
-  verbose=true,
   title="warmup_hunt_mpi",
-  solver=:petsc,
-  BL_adapted = true,
-  petsc_options="-snes_monitor -ksp_error_if_not_converged true -ksp_converged_reason -ksp_type preonly -pc_type lu -pc_factor_mat_solver_type mumps",
-  kmap_x=1,
-  kmap_y=1,
-  res_assemble=true,
-  jac_assemble=true,
-  solve=true
+  solver=:petsc
 )
 
-expansion(
-  mesh="710",
-  Ha = 10.0,
-  N = 3000.0,
-  cw = 0.01,
-  title="warmup_gmsh"
-)
+expansion(title="warmup_gmsh")
 expansion(np=2,backend=:sequential,title="warmup_gmsh_np2")
 expansion(np=1,backend=:mpi,title="warmup_gmsh_mpi")
