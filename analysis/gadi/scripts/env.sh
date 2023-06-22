c0d1bc099db1db6c25e1fe8ce47a9489bac0b3b8
Javier Principe <principe@cimne.upc.edu>
2023-06-23 02:33:57 +1000

    Minor fix to Gadi scripts

--- analysis/gadi/scripts/env.sh
+++ analysis/gadi/scripts/env.sh
@@ -2,8 +2,8 @@ module load openmpi/4.1.2
 module load intel-mkl/2022.0.2
 module load julia
 #export PATH=${HOME}/bin:$PATH # add (link to) julia executable here
-export PETSCROOT=${HOME}petsc-3.18.4
-export JULIA_PETSC_LIBRARY=${HOME}petsc-3.18.4/gcc-8.5.0_opmpi-4.1.2_mkl-2022.0.2_opt/lib/libpetsc.so
+export PETSCROOT=${HOME}/petsc-3.18.4
+export JULIA_PETSC_LIBRARY=${HOME}/petsc-3.18.4/gcc-8.5.0_opmpi-4.1.2_mkl-2022.0.2_opt/lib/libpetsc.so
 export JULIA_MPI_BINARY=system
 export JULIA_MPI_PATH=/apps/openmpi/4.1.2
 export GRIDAPMHD=${HOME}/GridapMHD.jl
