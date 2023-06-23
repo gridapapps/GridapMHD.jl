99c6ee81db16efc7ed4d5e514d1c36483e47682b
JordiManyer <jordi.manyer@monash.edu>
2023-06-23 16:54:57 +1000

    Added upper triangular preconditoner

--- test/BlockSolverTests.jl
+++ test/BlockSolverTests.jl
@@ -3,7 +3,88 @@ using Gridap, GridapDistributed, GridapSolvers
 using Gridap.MultiField
 using Gridap.Algebra
 
+using LinearAlgebra, BlockArrays
+
+using GridapSolvers.LinearSolvers: allocate_col_vector, allocate_row_vector
+
 using GridapMHD: _hunt, add_default_params, _fluid_mesh, weak_form, _find_backend, p_conformity
+
+struct MHDBlockPreconditioner <: Gridap.Algebra.LinearSolver
+  Dj_solver
+  Fu_solver
+  Δp_solver
+  Dj
+  Δp
+  params
+end
+
+struct MHDBlockPreconditionerSS <: Gridap.Algebra.SymbolicSetup
+  solver
+end
+
+function Gridap.Algebra.symbolic_setup(solver::MHDBlockPreconditioner, A::AbstractMatrix)
+  return MHDBlockPreconditionerSS(solver)
+end
+
+mutable struct MHDBlockPreconditionerNS <: Gridap.Algebra.NumericalSetup
+  solver
+  Dj_ns
+  Fu_ns
+  Δp_ns
+  sysmat
+  caches
+end
+
+function allocate_preconditioner_caches(solver,A::BlockMatrix)
+  du = allocate_col_vector(A[Block(1,1)])
+  dj = allocate_col_vector(A[Block(3,3)])
+  return du, dj
+end
+
+function Gridap.Algebra.numerical_setup(ss::MHDBlockPreconditionerSS, A::BlockMatrix)
+  solver = ss.solver
+  Fu = A[Block(1,1)]
+  Dj_ns = numerical_setup(symbolic_setup(solver.Dj_solver,solver.Dj),solver.Dj)
+  Fu_ns = numerical_setup(symbolic_setup(solver.Fu_solver,Fu),Fu)
+  Δp_ns = numerical_setup(symbolic_setup(solver.Δp_solver,solver.Δp),solver.Δp)
+  caches = allocate_preconditioner_caches(solver,A)
+  return MHDBlockPreconditionerNS(ss.solver,Dj_ns,Fu_ns,Δp_ns,A,caches)
+end
+
+function Gridap.Algebra.numerical_setup!(ns::MHDBlockPreconditionerNS, A::BlockMatrix)
+  numerical_setup!(ns.Fu_ns,A[Block(1,1)])
+  ns.sysmat = A
+end
+
+function Gridap.Algebra.solve!(x::BlockVector,ns::MHDBlockPreconditionerNS,b::BlockVector)
+  sysmat, caches, params = ns.sysmat, ns.caches, ns.solver.params
+  fluid = params[:fluid]; α = fluid[:α]; iRe = fluid[:β]
+  κ = 1.0; α1 = α + iRe; τ = 1.0
+
+  bu, bp, bj, bφ = blocks(b)
+  u, p, j, φ = blocks(x)
+  du, dj = caches
+
+  # Solve for φ and p
+  φ .= (-1.0/κ) .* bφ
+  solve!(p,ns.Δp_ns,bp); p .= -α1 .* bp + (2.0/τ) .* p;
+
+  # Solve for u
+  copy!(du,bu); mul!(du,sysmat[Block(1,2)],p,-1.0,1.0) # du = bu - Aup * p
+  solve!(u,ns.Fu_ns,du) # u = Fu \ (bu - Aup * p)
+
+  # Solve for j
+  copy!(dj,bj)
+  mul!(dj,sysmat[Block(3,1)],u,-2.0,1.0) # dj = bj - 2.0 * Aju * u
+  mul!(dj,sysmat[Block(3,4)],φ,-2.0,1.0) # dj = bj - 2.0 * Aju * u - 2.0 * Ajφ * φ
+  solve!(j,ns.Dj_ns,dj) # j = Dj \ (bj - 2.0 * Aju * u - 2.0 * Ajφ * φ)
+
+  return x
+end
+
+function get_identity_block(U::FESpace)
+  return LinearAlgebra.I(num_free_dofs(U))
+end
 
 function hunt(;
   backend=nothing,
@@ -65,11 +146,70 @@ U_p = TrialFESpace(V_p)
 U_φ = TrialFESpace(V_φ)
 U = MultiFieldFESpace([U_u,U_p,U_j,U_φ];style=BlockMultiFieldStyle())
 
+# Weak form
+
 res, jac = weak_form(params,k)
 Tm = params[:matrix_type]
 Tv = params[:vector_type]
 assem = SparseMatrixAssembler(Tm,Tv,U,V)
 op = FEOperator(res,jac,U,V,assem)
-xh = zero(U)
 
-r = residual(op,xh)
+# Preconditioner
+
+dΩj = Measure(get_triangulation(U_j),2*(k-1)+1)
+Dj = assemble_matrix((j,v_j) -> ∫(j⋅v_j + (∇⋅j)⋅(∇⋅v_j))*dΩj ,U_j,V_j)
+
+dΩp = Measure(get_triangulation(U_p),k-1)
+Δp  = assemble_matrix((p,v_p) -> ∫(∇(p)⋅∇(v_p))*dΩp ,U_p,V_p)
+
+Dj_solver = BackslashSolver()
+Fu_solver = BackslashSolver()
+Δp_solver = BackslashSolver()
+
+P = MHDBlockPreconditioner(Dj_solver,Fu_solver,Δp_solver,Dj,Δp,params)
+sysmat_solver = GMRESSolver(30,P,1e-6)
+
+# Solve
+
+xh = zero(U)
+sysvec = residual(op,xh)
+sysmat = jacobian(op,xh)
+
+Pns = numerical_setup(symbolic_setup(P,sysmat),sysmat)
+numerical_setup!(Pns,sysmat)
+
+sysmat_ns = numerical_setup(symbolic_setup(sysmat_solver,sysmat),sysmat)
+numerical_setup!(sysmat_ns,sysmat)
+
+x  = allocate_col_vector(sysmat)
+dx = allocate_col_vector(sysmat)
+
+solve!(x,sysmat_ns,sysvec)
+
+abstol = 1e-6
+for nliter in 1:10
+  println("  > Newton Iteration: ", nliter)
+
+  # Global linear solver
+  println("    > Update linear solver: ")
+  sysmat_solver = GMRESSolver(30,P,1e-6)
+  sysmat_ns     = numerical_setup(symbolic_setup(sysmat_solver,sysmat),sysmat)
+
+  # Solve linearized problem
+  println("    > Solver linearized problem:")
+  rmul!(sysvec,-1)
+  solve!(dx,sysmat_ns,sysvec)
+  x .+= dx
+
+  # Check convergence for the current residual
+  xh = FEFunction(X,x)
+  assemble_vector!(v->res(xh,v),sysvec,assem,Y)
+  res_norm = norm(sysvec)
+  (res_norm < abstol) && break;
+  println("    > Residual: ", res_norm)
+
+  # Assemble jacobian (fast in-place version)
+  # and prepare solver
+  assemble_matrix!((u,v)->jac(xh,u,v),sysmat,assem,X,Y)
+
+end
