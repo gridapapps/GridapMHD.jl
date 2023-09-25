21bc0c139960204e3da4921c0b1c861f5ce5c832
JordiManyer <jordi.manyer@monash.edu>
2023-09-25 17:19:02 +1000

    Rotate mesh axes

--- test/dev/expansion_p4est_mesh.jl
+++ test/dev/expansion_p4est_mesh.jl
@@ -5,29 +5,30 @@ using Gridap, GridapP4est
 
 using Gridap.Arrays, Gridap.Geometry, Gridap.ReferenceFEs
 
-function stretch(x::VectorValue{3})
-  α = 1.0
-  β = 2.0
-  γ = 0.2
-  return VectorValue{3}(α⋅x[1],β⋅x[2],γ⋅x[3])
-end
-
 function generate_base_mesh()
+  function rotate(x::VectorValue{3},p)
+    return VectorValue{3}(x[p[1]],x[p[2]],x[p[3]])
+  end
+  function stretch(x::VectorValue{3})
+    α = 1.0; β = 2.0; γ = 0.2;
+    return VectorValue{3}(α⋅x[1],β⋅x[2],γ⋅x[3])
+  end
+
   vertices = [
     (0,0,0),(1,0,0),(0,1,0),(1,1,0), # Main block, 1st layer
     (0,0,1),(1,0,1),(0,1,1),(1,1,1), # Main block, 2nd layer
     (0,0,2),(1,0,2),(0,1,2),(1,1,2), # Main block, 3rd layer
     (0,0,3),(1,0,3),(0,1,3),(1,1,3), # Main block, 4th layer
-    (0,2,1),(1,2,1),                 # Extension,  2nd layer
-    (0,2,2),(1,2,2),                 # Extension,  3rd layer
+    (0,-1,1),(1,-1,1),               # Extension,  2nd layer
+    (0,-1,2),(1,-1,2),               # Extension,  3rd layer
   ]
-  vertices = map(x -> stretch(VectorValue{3,Float64}(x...)),vertices)
+  vertices = map(x -> rotate(stretch(VectorValue{3,Float64}(x...)),[2,3,1]),vertices)
 
   c2n_map = Table([
     [ 1, 2, 3, 4, 5, 6, 7, 8],
     [ 5, 6, 7, 8, 9,10,11,12],
     [ 9,10,11,12,13,14,15,16],
-    [ 7, 8,17,18,11,12,19,20]
+    [17,18, 5, 6,19,20, 9,10]
   ])
 
   cell_type = [1,1,1,1]; polys = [HEX]; orientation = Oriented()
