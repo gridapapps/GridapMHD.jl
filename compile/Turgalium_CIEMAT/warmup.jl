d4202aea7a976d8ccef6235bf697eee22bcfe6c7
FRUrgorri <fernando.roca@ciemat.es>
2023-11-08 10:00:32 +0100

    update of warmup.jl to include the Fully developed driver in the compilatioupdate of warmup.jl to include the Fully developed driver in the compilationn

--- compile/Turgalium_CIEMAT/warmup.jl
+++ compile/Turgalium_CIEMAT/warmup.jl
@@ -1,5 +1,6 @@
 using GridapMHD: hunt
 using GridapMHD: expansion
+using GridapMHD: FullyDeveloped
 
 hunt(
   nc=(4,4),
@@ -29,7 +30,7 @@ hunt(
   title="warmup_hunt_petsc",
   solver=:petsc,
   BL_adapted = true,
-  petsc_options="-snes_monitor -ksp_error_if_not_converged true -ksp_converged_reason -ksp_type preonly -pc_type lu -pc_factor_mat_solver_type mumps -mat_mumps_icntl_4 2  -mat_mumps_icntl_28 2  -mat_mumps_icntl_29 2"
+  petsc_options="-snes_monitor -ksp_error_if_not_converged true -ksp_converged_reason -ksp_type preonly -pc_type lu -pc_factor_mat_solver_type mumps -mat_mumps_icntl_28 1 -mat_mumps_icntl_29 2 -mat_mumps_icntl_4 3 -mat_mumps_cntl_1 0.001"
 )
 
 hunt(
@@ -47,7 +48,7 @@ hunt(
   title="warmup_hunt_mpi",
   solver=:petsc,
   BL_adapted = true,
-  petsc_options="-snes_monitor -ksp_error_if_not_converged true -ksp_converged_reason -ksp_type preonly -pc_type lu -pc_factor_mat_solver_type mumps -mat_mumps_icntl_4 2  -mat_mumps_icntl_28 2  -mat_mumps_icntl_29 2"
+  petsc_options="-snes_monitor -ksp_error_if_not_converged true -ksp_converged_reason -ksp_type preonly -pc_type lu -pc_factor_mat_solver_type mumps -mat_mumps_icntl_28 1 -mat_mumps_icntl_29 2 -mat_mumps_icntl_4 3 -mat_mumps_cntl_1 0.001"
 )
 
 expansion(
@@ -59,6 +60,24 @@ expansion(
   vtk=true,
   title="warmup_gmsh_petsc",
   solver=:petsc,
-  petsc_options="-snes_monitor -ksp_error_if_not_converged true -ksp_converged_reason -ksp_type preonly -pc_type lu -pc_factor_mat_solver_type mumps -mat_mumps_icntl_4 2  -mat_mumps_icntl_28 2  -mat_mumps_icntl_29 2"
+  petsc_options="-snes_monitor -ksp_error_if_not_converged true -ksp_converged_reason -ksp_type preonly -pc_type lu -pc_factor_mat_solver_type mumps -mat_mumps_icntl_28 1 -mat_mumps_icntl_29 2 -mat_mumps_icntl_4 3 -mat_mumps_cntl_1 0.001"
 )
 
+FullyDeveloped(
+  parts=nothing,
+  nc=(4,4),
+  Ha = 10.0,
+  dir_B=(0.0,1.0,0.0),
+  b=1.0,
+  vtk=true,
+  title="test",
+  path=".",
+  debug=false,
+  solver=:petsc,
+  verbose=true,
+  cw_Ha = 0.01,
+  cw_s = 0.01,
+  τ_Ha = 1e6,
+  τ_s = 1e6,
+  petsc_options="-snes_monitor -ksp_error_if_not_converged true -ksp_converged_reason -ksp_type preonly -pc_type lu -pc_factor_mat_solver_type mumps -mat_mumps_icntl_28 1 -mat_mumps_icntl_29 2 -mat_mumps_icntl_4 3 -mat_mumps_cntl_1 0.001"
+)
