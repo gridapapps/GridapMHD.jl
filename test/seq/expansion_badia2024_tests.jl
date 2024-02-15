9213a3965be50cc122005959c7b0abfa3e9e39d0
JordiManyer <jordi.manyer@monash.edu>
2024-02-15 17:47:26 +0100

    Better tests

---
+++ test/seq/expansion_badia2024_tests.jl
@@ -0,0 +1,34 @@
+
+using GridapMHD: expansion
+using GridapPETSc, SparseMatricesCSR
+cw = 0.028
+Re = 1.0
+Ha = 100.0
+N = Ha^2/Re
+
+mesh = Dict(
+  :mesher => :MG,
+  :base_mesh => "coarse",
+  :num_refs_coarse => 0,
+  :ranks_per_level => [1,1],
+)
+solver = Dict(
+  :solver        => :badia2024,
+  :matrix_type   => SparseMatrixCSR{0,PetscScalar,PetscInt},
+  :vector_type   => Vector{PetscScalar},
+  :block_solvers => [:julia,:cg_jacobi,:cg_jacobi],
+  :petsc_options => "-ksp_error_if_not_converged true -ksp_converged_reason"
+)
+expansion(np=1,backend=:mpi,mesh=mesh,solver=solver,order=2,ζ=10000.0,N=N,Ha=Ha,cw=cw,title="ExpansionGMG")
+
+# ReferenceSolution
+expansion(np=1,backend=:mpi,solver=:julia,order=2,ζ=0.0,N=N,Ha=Ha,cw=cw,title="ExpansionRef")
+
+expansion(np=1,backend=:mpi,solver=:julia,order=2,ζ=0.0,N=N,Ha=Ha,cw=cw,title="ExpansionRefBis",mesh=mesh)
+
+meshbis = Dict(
+  :mesher => :SG,
+  :base_mesh => "coarse",
+  :num_refs => 1
+)
+expansion(np=1,backend=:mpi,solver=:julia,order=2,ζ=0.0,N=N,Ha=Ha,cw=cw,title="ExpansionRefBis",mesh=meshbis)
--- test/seq/hunt_tests.jl
+++ test/seq/hunt_tests.jl
@@ -2,7 +2,7 @@ module HuntTestsSequential
 
 using GridapMHD: hunt
 using GridapPETSc, SparseMatricesCSR
-"""
+
 hunt(
   nc=(4,4),
   L=1.0,
@@ -11,17 +11,17 @@ hunt(
   vtk=true,
   title="hunt",
   solver=:julia,
- )
+)
 
 hunt(
-  nc=(4,4),
+  nc=(10,10),
   L=1.0,
   B=(0.,50.,0.),
   debug=false,
   vtk=true,
   title="hunt",
   solver=:julia,
- )
+)
 
 hunt(
   nc=(4,4),
@@ -33,7 +33,7 @@ hunt(
   vtk=true,
   title="hunt",
   solver=:julia,
- )
+)
 
 hunt(
   nc=(4,4),
@@ -45,7 +45,7 @@ hunt(
   vtk=true,
   title="hunt",
   solver=:julia,
- )
+)
 
 hunt(
   nc=(4,4),
@@ -57,92 +57,6 @@ hunt(
   vtk=true,
   title="hunt",
   solver=:petsc,
-)
-"""
-
-"""
-hunt(
-  nc=(4,4),
-  np=(2,2),
-  backend=:sequential,
-  L=1.0,
-  B=(0.,50.,0.),
-  debug=false,
-  vtk=true,
-  title="hunt",
-  solver=:li2019,
-)
-"""
-
-solver = Dict(
-  :solver         => :badia2024,
-  :matrix_type    => SparseMatrixCSR{0,PetscScalar,PetscInt},
-  :vector_type    => Vector{PetscScalar},
-  :petsc_options  => "-ksp_error_if_not_converged true -ksp_converged_reason",
-  :block_solvers  => [:petsc_gmres_schwarz,:petsc_cg_jacobi,:petsc_cg_jacobi],
-)
-hunt(
-  nc=(4,4),
-  np=(2,2),
-  backend=:sequential,
-  L=1.0,
-  B=(0.,50.,0.),
-  debug=false,
-  vtk=true,
-  title="hunt",
-  solver=solver,
-)
-
-# Li2019, MUMPS for Dj
-solver = Dict(
-  :solver        => :li2019,
-  :matrix_type   => SparseMatrixCSR{0,PetscScalar,PetscInt},
-  :vector_type   => Vector{PetscScalar},
-  :block_solvers => [:petsc_gmres_schwarz,:petsc_gmres_schwarz,:petsc_cg_jacobi,:petsc_cg_jacobi],
-  :petsc_options => "-ksp_error_if_not_converged true -ksp_converged_reason"
-)
-hunt(
-  nc=(4,4),
-  np=(2,2),
-  backend=:sequential,
-  L=1.0,
-  B=(0.,50.,0.),
-  debug=false,
-  vtk=true,
-  title="hunt",
-  solver=solver,
-)
-
-# Li2019, GMRES + Overlapping Additive Schwarz preconditioner for Dj
-petsc_options = """
-  -ksp_type gmres
-  -ksp_rtol 1.0e-5
-  -ksp_atol 1.0e-14
-  -ksp_converged_reason
-  -pc_type asm
-  -pc_asm_overlap 10
-  -pc_asm_type restrict
-  -pc_asm_blocks 32
-  -sub_ksp_type preonly
-  -sub_pc_type lu
-"""
-solver = Dict(
-  :solver        => :li2019,
-  :matrix_type   => SparseMatrixCSR{0,PetscScalar,PetscInt},
-  :vector_type   => Vector{PetscScalar},
-  :block_solvers => [:petsc_from_options,:petsc_gmres_schwarz,:petsc_cg_jacobi,:petsc_cg_jacobi],
-  :petsc_options => petsc_options
-)
-hunt(
-  nc=(4,4),
-  np=(2,2),
-  backend=:sequential,
-  L=1.0,
-  B=(0.,50.,0.),
-  debug=false,
-  vtk=true,
-  title="hunt",
-  solver=solver,
 )
 
 end # module
