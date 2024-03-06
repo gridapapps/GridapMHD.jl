64855e2b2f6505787ab27b33a00dbe8dc4b34fd0
JordiManyer <jordi.manyer@monash.edu>
2024-03-07 10:31:46 +1100

    Fixed MPI tests

--- test/mpi/runtests.jl
+++ test/mpi/runtests.jl
@@ -3,15 +3,19 @@ module GridapMHDMPITests
 using Test
 using MPI
 
+tests_to_run = ["cavity_tests.jl","hunt_tests.jl","expansion_tests.jl"]
+
 testdir = @__DIR__
-istest(f) = endswith(f, ".jl") && !(f=="runtests.jl")
+istest(f) = endswith(f, ".jl") && !(f=="runtests.jl") && (f in tests_to_run)
 testfiles = sort(filter(istest, readdir(testdir)))
 
 MPI.mpiexec() do cmd
   for file in testfiles
     path = joinpath(testdir,file)
-    _cmd = `$(cmd) -np 4 julia --project=. $path`
+    _cmd = `$(cmd) -np 4 --allow-run-as-root --oversubscribe $(Base.julia_cmd()) --project=. $path`
+    @show cmd
     run(_cmd)
+    @test true
   end
 end
 
