bf2f6d922d9f5a68477005bc8920ca39bbd8b17f
FRUrgorri <fernando.roca@ciemat.es>
2024-06-20 11:48:52 +0200

    Update of the analysis/xula/env.sh to downgrade the version of petsc

--- analysis/xula/env.sh
+++ analysis/xula/env.sh
@@ -1,10 +1,10 @@
 module load gcc-13.1.0-gcc-8.5.0-ueufrru
 module load openmpi-4.1.5-gcc-13.1.0-nzcz542 
 
-export PETSCROOT=/mnt/lustre/home/u6678/software/petsc/3.20.2
-export JULIA_PETSC_LIBRARY=/mnt/lustre/home/u6678/software/petsc/3.20.2/lib/libpetsc.so
-export JULIA_MPI_BINARY=system
-export JULIA_MPI_PATH=/mnt/lustre/home/spack/spack/opt/spack/linux-rocky8-icelake/gcc-13.1.0/openmpi-4.1.5-nzcz542rea5aajaza2x6ps3ym4krutku/
+export PETSCROOT=/mnt/lustre/home/u6678/software/petsc/3.15.4
+export JULIA_PETSC_LIBRARY=/mnt/lustre/home/u6678/software/petsc/3.15.4/lib/libpetsc.so
+#export JULIA_MPI_BINARY=system
+#export JULIA_MPI_PATH=/mnt/lustre/home/spack/spack/opt/spack/linux-rocky8-icelake/gcc-13.1.0/openmpi-4.1.5-nzcz542rea5aajaza2x6ps3ym4krutku/
 export GMSHROOT=/mnt/lustre/home/u6678/software/gmsh/4.11.1
 export GRIDAPMHD=/mnt/lustre/home/u6678/work/GridapMHD.jl
 
